9020bd8eede40de6d3e9ecd6ec3524fbaaf91014
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2024-09-11 17:59:52 +0100

    "Remove usePageContext and Cookies import, and onMouseOver event handler from Home component"

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -5,18 +5,10 @@ import Firstsection from '@/components/firstsection';
 import { Footer } from '@/components/footer';
 import Header from '@/components/header';
 import { SkillsSection } from '@/components/skillssection';
-import { usePageContext } from '@/contexts/pageContext';
-import Cookies from 'js-cookie';
 
 export default function Home() {
-  const { setTheme } = usePageContext();
   return (
-    <main
-      onMouseOver={() => {
-        const selectedTheme = Cookies.get('theme');
-        setTheme(selectedTheme);
-      }}
-      className={`min-h-[100vh] transition duration-500`}>
+    <main className={`min-h-[100vh] transition duration-500`}>
       <Header />
       <Firstsection />
       <SkillsSection />
