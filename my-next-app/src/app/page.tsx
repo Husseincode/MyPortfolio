c1231b5a0ddc082fdcc7d55e19f93867887dfbcb
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2024-09-11 15:57:30 +0100

    "Removed theme context usage in Home component and refactored PageContext to retrieve and store theme in local storage"

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -5,12 +5,10 @@ import Firstsection from '@/components/firstsection';
 import { Footer } from '@/components/footer';
 import Header from '@/components/header';
 import { SkillsSection } from '@/components/skillssection';
-import { usePageContext } from '@/contexts/pageContext';
 
 export default function Home() {
-  const { theme } = usePageContext();
   return (
-    <main className={`min-h-[100vh] ${theme} transition duration-500`}>
+    <main className={`min-h-[100vh] transition duration-500`}>
       <Header />
       <Firstsection />
       <SkillsSection />
--- my-next-app/src/contexts/pageContext.tsx
+++ my-next-app/src/contexts/pageContext.tsx
@@ -1,14 +1,42 @@
 /** @format */
 
 'use client';
-import React, { useState, useContext, createContext, ReactNode } from 'react';
+import React, {
+  useState,
+  useContext,
+  createContext,
+  ReactNode,
+  useEffect,
+} from 'react';
 import { GlobalPageContext } from '@/types/GlobalPageContext';
 
 const PageContextAPI = createContext<GlobalPageContext | undefined>(undefined);
 
 export const PageContext = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<string>('light');
+  const [theme, setTheme] = useState<string>(() => {
+    // Retrieve theme from local storage or default to 'light'
+    const storedTheme = localStorage.getItem('theme');
+    return storedTheme ? storedTheme : 'light';
+  });
   const [email, setEmail] = useState<string>('');
+
+  {
+    /**use Effects */
+  }
+  useEffect(() => {
+    // Retrieve theme from local storage
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+  }, []);
+  useEffect(() => {
+    // Apply the theme to the document body
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(theme);
+    // Save the theme to local storage
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (
     <PageContextAPI.Provider value={{ theme, setTheme, email, setEmail }}>
