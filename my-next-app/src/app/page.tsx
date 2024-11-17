53415e3944a955660fcd72d54443ffb155c7d226
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2024-11-17 07:14:22 +0100

    commenting out Contact Us Section

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <Header />
       <Firstsection />
       <SkillsSection />
-      <ContactUs />
+      {/* <ContactUs /> */}
       <Footer />
     </main>
   );
