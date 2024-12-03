192935d082b21fda389b3dc6d25fb3bacc6807af
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2024-12-03 04:01:53 +0100

    Added `overflow-hidden` class to two button elements in `HeaderComponent`.

--- my-next-app/src/components/header.tsx
+++ my-next-app/src/components/header.tsx
@@ -84,7 +84,7 @@ const HeaderComponent = () => {
             <button
               type='button'
               onClick={changeTheme}
-              className='group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition cursor-pointer'>
+              className='group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition cursor-pointer overflow-hidden'>
               {' '}
               <svg
                 viewBox='0 0 24 24'
@@ -105,7 +105,7 @@ const HeaderComponent = () => {
             <button
               type='button'
               onClick={changeTheme}
-              className='group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20 cursor-pointer'>
+              className='group rounded-full bg-white/90 px-3 py-2 shadow-lg overflow-hidden shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20 cursor-pointer'>
               {' '}
               <svg
                 viewBox='0 0 24 24'
